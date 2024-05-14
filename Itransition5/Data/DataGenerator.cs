2f03c5d8eb1c896ebd459ad6e6b60e05b425277f
JavohirAliyev <jakhaaliyev@gmail.com>
2024-05-14 11:36:02 +0500

    first commit

---
+++ Itransition5/Data/DataGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+
+
+namespace Itransition5.Data
+{
+	public class DataGenerator
+	{
+		Faker<PersonModel> faker;
+
+        public DataGenerator()
+        {
+            Randomizer.Seed = new Random(123);
+
+            faker = new Faker<PersonModel>("it")
+                .RuleFor(u => u.Id, f => Guid.NewGuid())
+                .RuleFor(u => u.Name, f => f.Name.FullName())
+                .RuleFor(u => u.Address, f => f.Address.FullAddress())
+                .RuleFor(u => u.Phone, f => f.Phone.PhoneNumber());
+        }
+        public PersonModel GeneratePerson()
+        {
+            
+            return faker.Generate();
+        }
+    }
+}
---
+++ Itransition5/Data/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Itransition5.Data
+{
+    public record PersonModel()
+    {
+        public Guid Id { get; set; }
+        public string Name {  get; set; }
+        public string Address {  get; set; }
+        public string Phone {  get; set; }
+    }
+
+
+}
