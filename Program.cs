22dab07d440aa1d2425a5b46649f53ca082dcb91
JavohirAliyev <jakhaaliyev@gmail.com>
2025-10-13 21:31:39 +0500

    recovered bootstrap styling

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-app.MapBlazorHub();
 app.UseStaticFiles();
+
+app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
